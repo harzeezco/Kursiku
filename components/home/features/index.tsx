c4f8d5f1d4cf88e36c83e3fb98b9853ddc590445
harzeezco <wakilabdulazeez@gmail.com>
2023-12-09 14:31:47 +0100

    building the ui -> finish the left side of the layout, the second side needs some touching with the image

--- components/home/features/index.tsx
+++ components/home/features/index.tsx
@@ -21,20 +21,20 @@ const FEATURESDETAILS = [
 ];
 
 function Features() {
-  const background = '#103C44 no-repeat center center';
+  const background = "#103C44 url('/png/home/bg.png') no-repeat center center";
 
   return (
     <section
-      className='features-container relative mt-24'
+      className='mt-24'
       style={{ background, width: '100%', height: '100%' }}
     >
-      <div className='container relative flex items-center justify-between gap-8 p-20'>
+      <div className='container relative flex flex-col items-center justify-between gap-8 p-20 md:flex-row'>
         <div>
           <h1 className='h2-bold mx-auto mb-3 text-white lg:max-w-[550px]'>
             Put expenses on easy mode and{' '}
             <span className='text-orange-500'>save tons of money</span>
           </h1>
-          <ul className='mt-8 grid gap-7 text-white'>
+          <ul className='mt-20 grid gap-7 text-white'>
             {FEATURESDETAILS.map((details, idx) => (
               <li className='grid grid-cols-[4rem_1fr] place-content-center gap-8'>
                 <div className='grid h-20 w-20 place-items-center rounded-full bg-green-800 text-2xl font-semibold text-white'>
@@ -50,9 +50,9 @@ function Features() {
             ))}
           </ul>
         </div>
-        <div className='grid place-items-end'>
+        <div className='grid place-items-start lg:place-items-end'>
           <Button variant='primary'>
-            <span className='text-[22px] font-semi-bold'>Get Started</span>
+            <span className='text-[20px] font-semibold'>Get Started</span>
           </Button>
           <Image
             src='/png/home/image.png'
